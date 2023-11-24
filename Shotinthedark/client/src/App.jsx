f30d9a998a29c763d8669d984dec1a315d5462c6
Armandosamarbide <martin.salduna@gmail.com>
2023-11-24 19:29:29 -0300

    Intento de trabajo final y otras cosas

--- Shotinthedark/client/src/App.jsx
+++ Shotinthedark/client/src/App.jsx
@@ -1,8 +1,28 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Axios from 'axios'
+/* import { response } from 'express' */
 
 function App() {
 
+const [movieName, setMovieName] = useState('')
+const [review, setReview] = useState('')
+/* const [movieReviewList, setMovieList] = useState('') */
+
+/* useEffect(() =>{
+Axios.get('http://localhost:5174/api/get').then ((response) => {
+setMovieList(response.data)
+})
+}, []) */
+
+const submitReview = () => {
+Axios.post('http://localhost:5174/api/insert', {
+  movieName: movieName, 
+  movieReview: review,
+}).then(()=> {
+  alert('Nuevo contenido añadido con éxito')
+})
+}
 
   return (
     <>
@@ -11,12 +31,26 @@ function App() {
 
       <div className='form'>
       <label>Movie Name:</label>
-      <input type="text" name="movieName" />
+      <input type="text" name="movieName" onChange={(e)=>{
+        setMovieName(e.target.value)
+      }}/>
       
       <label>Review:</label>
-      <input type="text" name="review" />
+      <input type="text" name="review" onChange={(e)=>{
+        setReview(e.target.value)
+      }}/>
 
-      <button>Submit</button>
+      <button onClick={submitReview}>Submit</button>
+      
+     {/*  <div>
+      {movieReviewList.map((value) => {
+      return <h2>Movie: {value.movieName} | Review: {value.movieReview}</h2>
+
+            }
+      )}
+      
+      </div> */}
+      
       </div>
       
       </div>
--- Shotinthedark/server/index.js
+++ Shotinthedark/server/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const mysql = require('mysql')
 
 const app = express()
@@ -10,13 +12,41 @@ const db = mysql.createConnection({
     database: 'cruddatabase'
 })
 
-app.get('/', (req, res)=>{
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+/* Ver todos los elementos de la tabla */
+
+/* app.get('/api/get', (req,res)=>{
+
+    const sqlSelect = "SELECT * FROM movie_reviews"
+    db.query(sqlSelect, (err,result) => {
+        res.send(result)
+    })
+}) */
+
+/* Creación de un nuevo elemento en la tabla  */
+
+app.post('/api/insert', (req,res)=>{
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName,movieReview) VALUES (?,?)"
+    db.query(sqlInsert, [movieName, movieReview], (err,result) => {
+        console.log(err)
+    })
+})
+
+
+
+/* app.get('/', (req, res)=>{
     const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Black Hawk Down', 'Kick ass');"
     db.query(sqlInsert, (err, result)=>{
     res.send('Success')
-    })
-    
-})
+    }) 
+}) */
 
 app.get('/', (req, res) => {
     res.send('This is the Frontend')
