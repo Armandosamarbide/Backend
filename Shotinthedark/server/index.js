36420cd18497ef49ac3353b85d994cbe23889c1e
Armandosamarbide <martin.salduna@gmail.com>
2023-11-24 15:47:28 -0300

    Intento de trabajo final y otras cosas

--- Shotinthedark/server/index.js
+++ Shotinthedark/server/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
-
-
+const mysql = require('mysql')
 
 const app = express()
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'cruddatabase'
+})
+
+app.get('/', (req, res)=>{
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Black Hawk Down', 'Kick ass');"
+    db.query(sqlInsert, (err, result)=>{
+    res.send('Success')
+    })
+    
+})
 
 app.get('/', (req, res) => {
     res.send('This is the Frontend')
